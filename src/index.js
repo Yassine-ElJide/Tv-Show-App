12a1b72c86d11aba106ed6c6bad821262f9bc6c0
Yassine-ElJide <yassine.eljide10@gmail.com>
2021-02-16 22:24:20 +0100

    tv-show

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+
+// State
+import ShowsState from "./context/shows/ShowsState";
+import AlertsState from "./context/alerts/AlertsState";
+
+ReactDOM.render(
+  <ShowsState>
+    <AlertsState>
+      <App />
+    </AlertsState>
+  </ShowsState>,
+  document.getElementById("root")
+);
